f1cddcc889293938c8241e56fd5776f5d2fd6a18
godwinnduka <136750604+gnduka@users.noreply.github.com>
2023-08-26 20:13:06 +0100

    Second check

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to the Cost Visualiser Program.");
-
+Console.WriteLine("To start, please enter your hourly rate");
