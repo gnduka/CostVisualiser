143ba499713585434472f76f43a0539e4c5655d3
godwinnduka <136750604+gnduka@users.noreply.github.com>
2023-08-26 20:34:56 +0100

    Implemented the ability for the user to enter the currency they wish to use

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to the Cost Visualiser Program.");
-Console.WriteLine("To start, please enter your hourly rate");
+Console.WriteLine("To start, please enter your currency symbol.");
+Console.WriteLine("Do not enter the full name of the currency you wish to use");
+
+string currencySymbol = Console.ReadLine();
+
+Console.WriteLine("Next, please enter your hourly rate");
 
 float hourlyRate = float.Parse(Console.ReadLine());
 
-Console.WriteLine("What is the cost of the item/service you wish to purchase?");
+Console.WriteLine("Finally, what is the cost of the item/service you wish to purchase?");
 
 float costOfProduct = float.Parse(Console.ReadLine());
 
